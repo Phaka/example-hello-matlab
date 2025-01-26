d77c481b668ee6fd97993adf9104504a4b0af9d0
Werner Strydom <bloudraak@users.noreply.github.com>
2025-01-26 07:21:06 -0800

    Create hello.m

---
+++ hello.m
@@ -0,0 +1,8 @@
+% Hello World in MATLAB
+% MATLAB uses % for comments and is specialized for matrix operations
+% Files must have the same name as their primary function
+% MATLAB combines a programming language with an interactive environment
+
+function hello()
+    disp('Hello, World!')
+end
